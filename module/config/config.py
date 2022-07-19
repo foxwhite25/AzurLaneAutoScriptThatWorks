4d5f8d01abf13eb6faa80ec4e0031dc6cbdc71cd
Fox_white <vct.xie@gmail.com>
2022-07-20 03:51:51 +0800

    Fix override options

--- module/config/config.py
+++ module/config/config.py
@@ -87,7 +87,7 @@ class AzurLaneConfig(ConfigUpdater, ManualConfig, GeneratedConfig, ConfigWatcher
         self.auto_update = True
         # Force override variables
         # Key: Argument name in GeneratedConfig. Value: Modified value.
-        self.overridden = {}
+        # self.overridden = {}
         # Scheduler queue, will be updated in `get_next_task()`, list of Function objects
         # pending_task: Run time has been reached, but haven't been run due to task scheduling.
         # waiting_task: Run time haven't been reached, wait needed.
@@ -271,22 +271,6 @@ class AzurLaneConfig(ConfigUpdater, ManualConfig, GeneratedConfig, ConfigWatcher
                     logger.warning(f"NextRun of task {task} is too far, reset to now")
                     deep_set(self.data, keys=f"{task}.Scheduler.NextRun", value=now)
 
-        def force_enable(tasks):
-            for task in tasks:
-                enable = deep_get(
-                    self.data, keys=f"{task}.Scheduler.Enable", default=None
-                )
-                if enable is not None and not enable:
-                    logger.warning(f"Task {task} is force to enable")
-                    self.modified[f"{task}.Scheduler.Enable"] = True
-
-        force_enable(
-            [
-                "Commission",
-                "Research",
-                "Reward",
-            ]
-        )
         limit_next_run(
             ["Commission", "Reward"], limit=now + timedelta(hours=12, seconds=-1)
         )
@@ -300,9 +284,7 @@ class AzurLaneConfig(ConfigUpdater, ManualConfig, GeneratedConfig, ConfigWatcher
         Variables stall remain overridden even config is reloaded from yaml file.
         Note that this method is irreversible.
         """
-        for arg, value in kwargs.items():
-            self.overridden[arg] = value
-            super().__setattr__(arg, value)
+        pass
 
     def set_record(self, **kwargs):
         """
